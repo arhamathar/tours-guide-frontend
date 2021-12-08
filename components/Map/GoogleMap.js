9208fd08c66d3686edac0a88e51debe4e53c08e3
yashmittal19 <yashmittal1906@gmail.com>
2021-12-08 12:21:51 +0530

    rendered map

---
+++ components/Map/GoogleMap.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+
+export const MapContainer = () =>{
+    const [showingInfoWindow,setShowingInfoWindow] = React.useState(false);
+    const [activeMarkers,setActiveMarkers] = React.useState(false);
+    
+
+    const onMarkerClick = () =>{
+        setShowingInfoWindow({
+        showingInfoWindow: true
+      });
+    }
+  
+    const onMapClicked = () => {
+      if (showingInfoWindow) {
+        setShowingInfoWindow({
+          showingInfoWindow: false,
+        })
+      }
+    };
+  
+      return (
+        <Map 
+        google={google}
+            style={{width: '50%', height: '130%'}}
+            onClick={onMapClicked}>
+          <Marker onClick={onMarkerClick}
+                  name={'Current location'} />
+  
+          <InfoWindow
+            marker={activeMarkers}
+            visible={showingInfoWindow}>
+              <div>
+                <h1>Hello</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      )
+  }
+
+
+
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyCryqdJrYXn4RAZ6LU_kC-uYB2Tn2K00_M")
+  })(MapContainer)
