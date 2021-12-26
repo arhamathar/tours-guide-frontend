db872fd391adaa0caf143cb63907e9892b6f4410
arhamathar <arhamathar789@gmail.com>
2021-12-26 23:28:41 +0530

    persistent login in lcoalStorage

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const useAuth = () => {
     const initialState = {
@@ -6,18 +6,27 @@ const useAuth = () => {
         email: null,
         token: null,
     };
+
     const [user, setUser] = useState(initialState);
-    const login = (name: string, email: string, token: string) => {
+
+    const login = useCallback((name: string, email: string, token: string) => {
         localStorage.setItem(
             'userData',
             JSON.stringify({ name, email, token }),
         );
-    };
+    }, []);
 
     const logout = () => {
         setUser((prev) => ({ ...prev, ...initialState }));
         localStorage.removeItem('userData');
     };
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('userData') || '');
+        if (storedData && storedData.token) {
+            login(storedData.userId, storedData.token, storedData.expiration);
+        }
+    }, [login]);
 
     return { user, login, logout };
 };
