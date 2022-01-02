7ba03511f81db2f6b3e802a51c55c6a5a3715e6c
arhamathar <arhamathar789@gmail.com>
2022-01-02 20:30:04 +0530

    basic admin routes

---
+++ components/admin/adminRoutes.ts
@@ -0,0 +1,30 @@
+const adminRoutes = {
+    hotels: {
+        path: '/admin/hotels',
+        access: ['admin', 'manager'],
+        navbar: [
+            { title: 'Add', path: '/admin/hotel/create' },
+            { title: 'Update', path: '/admin/hotel/update' },
+            { title: 'Back', path: '/admin/hotels' },
+        ],
+    },
+    bookings: {
+        path: '/admin/bookings',
+        access: ['admin', 'manager', 'traveller'],
+        navbar: [
+            { title: 'All', path: '/admin/bookings/all' },
+            { title: 'Future', path: '/admin/bookings/future' },
+            { title: 'History', path: '/admin/bookings/history' },
+        ],
+    },
+    setting: {
+        path: '/admin/setting',
+        access: ['admin'],
+    },
+    register: {
+        path: '/admin/register/user',
+        access: ['admin'],
+    },
+};
+
+export default adminRoutes;
